ca8f22b691da7d956d8d6c592bfeb78f3d28ef43
fabian <fabian@haeger.info>
2024-10-07 16:26:09 +0200

    feat: Add message reading to cli

--- cli/client/client.go
+++ cli/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/coder/websocket"
@@ -18,6 +19,16 @@ func Connect(adress string, port string) {
 	c, _, err := websocket.Dial(ctx, fmt.Sprintf("ws://%s:%s/tunnler/connection/subscribe", adress, port), nil)
 	if err != nil {
 		c.Close(websocket.StatusInternalError, "")
+	}
+
+	for {
+		msgType, msgByte, err := c.Read(ctx)
+		if err != nil {
+			log.Fatalf("error in receiving message: %v", err)
+			break
+		}
+
+		fmt.Printf("Received %s // %s\n", msgType, msgByte)
 	}
 
 	defer c.CloseNow()
