da292d13778abcdd8c378416eb31bc7e2047cda2
fabian <fabian@haeger.info>
2024-10-10 20:03:28 +0200

    feat: Add LRU Cache Serialization and Deserialization

--- cache/lru.go
+++ cache/lru.go
@@ -2,6 +2,9 @@ package cache
 
 import (
 	"container/list"
+	"encoding/gob"
+	"io"
+	"os"
 	"sync"
 	"time"
 )
@@ -15,8 +18,8 @@ type LRUCache struct {
 }
 
 type entry struct {
-	key      string
-	value    string
+	Key      string
+	Value    string
 	size     int
 	lastUsed time.Time
 }
@@ -50,7 +53,7 @@ func (c *LRUCache) Get(key string) string {
 
 	if e := c.hash[key]; e != nil {
 		c.moveToFront(e)
-		return e.Value.(*entry).value
+		return e.Value.(*entry).Value
 	}
 
 	return ""
@@ -73,20 +76,90 @@ func (c *LRUCache) Clear() {
 	c.size = 0
 }
 
+func (c *LRUCache) Entries() []entry {
+	entries := []entry{}
+	for _, it := range c.hash {
+		entries = append(entries, *it.Value.(*entry))
+	}
+	return entries
+}
+
+func (c *LRUCache) Save(cacheFile string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	file, err := os.OpenFile(cacheFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+
+	return c.saveCache(file)
+}
+
+func (c *LRUCache) Load(cacheFile string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	file, err := os.OpenFile(cacheFile, os.O_RDONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	entries, err := c.loadFromReader(file)
+	if err != nil {
+		return err
+	}
+
+	if len(entries) > 0 {
+		c.setCapacity(len(entries))
+	}
+
+	for _, e := range entries {
+		c.Set(e.Key, e.Value)
+	}
+
+	return nil
+}
+
+func (c *LRUCache) saveCache(w io.Writer) error {
+	entries := c.Entries()
+
+	enc := gob.NewEncoder(w)
+	return enc.Encode(entries)
+}
+
+func (c *LRUCache) loadFromReader(r io.Reader) ([]entry, error) {
+	var entries []entry
+
+	dec := gob.NewDecoder(r)
+	if err := dec.Decode(&entries); err != nil {
+		return nil, err
+	}
+	return entries, nil
+}
+
 func (c *LRUCache) ensureCapacity() {
 	for c.size > c.capacity {
 		lastElem := c.list.Back()
 		lastValue := lastElem.Value.(*entry)
 		c.list.Remove(lastElem)
-		delete(c.hash, lastValue.key)
+		delete(c.hash, lastValue.Key)
 		c.size -= uint64(lastValue.size)
 	}
 }
 
+func (c *LRUCache) setCapacity(capacity int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.capacity = uint64(capacity)
+	c.ensureCapacity()
+}
+
 func (c *LRUCache) add(key string, value string) {
 	e := &entry{
-		key:      key,
-		value:    value,
+		Key:      key,
+		Value:    value,
 		size:     len(value),
 		lastUsed: time.Now().UTC(),
 	}
@@ -98,7 +171,7 @@ func (c *LRUCache) add(key string, value string) {
 func (c *LRUCache) update(e *list.Element, value string) {
 	size := len(value)
 	sizeDiff := size - e.Value.(*entry).size
-	e.Value.(*entry).value = value
+	e.Value.(*entry).Value = value
 	e.Value.(*entry).size = size
 	c.size += uint64(sizeDiff)
 }
--- cache/lru_test.go
+++ cache/lru_test.go
@@ -1,86 +1,181 @@
 package cache
 
 import (
+	"bytes"
 	"container/list"
+	"fmt"
 	"testing"
 )
 
 func TestLRUCreation(t *testing.T) {
-	lru := NewLRUCache(100)
+	tests := []struct {
+		capacity int
+	}{
+		{0},
+		{-1},
+		{5},
+		{100000},
+	}
 
-	if lru.capacity != 100 && lru.size != 0 {
-		t.Errorf("LRU isn't initialized correctly")
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Capacity %v", tt.capacity), func(t *testing.T) {
+			lru := NewLRUCache(uint64(tt.capacity))
+
+			if lru.capacity != uint64(tt.capacity) && lru.size != 0 {
+				t.Errorf("LRU isn't initialized correctly")
+			}
+		})
 	}
 }
 
 func TestLRUAddGet(t *testing.T) {
 	lru := NewLRUCache(10)
 
-	tests := []struct {
-		key   string
-		value string
-	}{
-		{"A", "a"},
-		{"B", "b"},
-		{"longer", "long"},
+	type kv struct {
+		Key   string
+		Value string
 	}
 
-	tsize := 0
+	type testData struct {
+		name     string
+		capacity int
+		KV       []kv
+		Expected []kv
+	}
+
+	tests := []testData{
+		testData{
+			name:     "Full Capacity",
+			capacity: 5,
+			KV: []kv{
+				kv{Key: "A", Value: "a"},
+				kv{Key: "B", Value: "b"},
+				kv{Key: "C", Value: "c"},
+				kv{Key: "D", Value: "d"},
+				kv{Key: "E", Value: "e"},
+			},
+			Expected: []kv{
+				kv{Key: "A", Value: "a"},
+				kv{Key: "B", Value: "b"},
+				kv{Key: "C", Value: "c"},
+				kv{Key: "D", Value: "d"},
+				kv{Key: "E", Value: "e"},
+			},
+		},
+		testData{
+			capacity: 3,
+			KV: []kv{
+				kv{Key: "A", Value: "a"},
+				kv{Key: "B", Value: "b"},
+				kv{Key: "C", Value: "c"},
+				kv{Key: "D", Value: "d"},
+				kv{Key: "E", Value: "e"},
+			},
+			Expected: []kv{
+				kv{Key: "C", Value: "c"},
+				kv{Key: "D", Value: "d"},
+				kv{Key: "E", Value: "e"},
+			},
+		},
+	}
+
 	for _, tt := range tests {
-		lru.Set(tt.key, tt.value)
-		tsize += len(tt.value)
-		if lru.size != uint64(tsize) {
-			t.Errorf("size of LRU (%v) doesn't match expected size of %v", lru.size, tsize)
-		}
-
-		v := lru.Get(tt.key)
-		if tt.value != v {
-			t.Errorf("value of LRU key %v returned %v. expected %v", tt.key, tt.value, v)
-		}
-
-		if e := lru.hash[tt.key]; e == nil {
-			t.Errorf("list element with key %v shouldn't be nil", tt.key)
-		} else {
-			if !elementShouldBeFirst(e) {
-				t.Errorf("list element with key %v should be first in the list", tt.key)
+		t.Run(tt.name, func(t *testing.T) {
+			tsize := 0
+			for _, v := range tt.KV {
+				lru.Set(v.Key, v.Value)
+				tsize += len(v.Value)
 			}
-		}
+
+			if lru.size != uint64(tsize) {
+				t.Errorf("size of LRU (%v) doesn't match expected size of %v", lru.size, tsize)
+			}
+
+			for _, e := range tt.KV {
+				v := lru.Get(e.Key)
+				if e.Value != v {
+					t.Errorf("value of LRU key %v returned %v. expected %v", e.Key, e.Value, v)
+				}
+
+				if e := lru.hash[e.Key]; e == nil {
+					t.Errorf("list element with key %v shouldn't be nil", e.Value)
+				} else {
+					if !elementShouldBeFirst(e) {
+						t.Errorf("list element with key %v should be first in the list", e.Value)
+					}
+				}
+			}
+		})
 	}
 }
 
 func TestLRUCapacity(t *testing.T) {
-	lru := NewLRUCache(3)
-
-	tests := []struct {
+	type testData []struct {
 		key          string
 		value        string
 		expectedKeys []string
-	}{
-		{"A", "a", []string{"A"}},
-		{"B", "b", []string{"A", "B"}},
-		{"C", "c", []string{"A", "B", "C"}},
-		{"D", "d", []string{"B", "C", "D"}},
-		{"E", "e", []string{"C", "D", "E"}},
+	}
+	type testsData struct {
+		data     testData
+		capacity int
+	}
+
+	tests := []testsData{
+		testsData{
+			capacity: 3,
+			data: testData{
+				{"A", "a", []string{"A"}},
+				{"B", "b", []string{"A", "B"}},
+				{"C", "c", []string{"A", "B", "C"}},
+				{"D", "d", []string{"B", "C", "D"}},
+				{"E", "e", []string{"C", "D", "E"}},
+			},
+		},
+		testsData{
+			capacity: 1,
+			data: testData{
+				{"A", "a", []string{"A"}},
+				{"B", "b", []string{"B"}},
+				{"C", "c", []string{"C"}},
+				{"D", "d", []string{"D"}},
+				{"E", "e", []string{"E"}},
+			},
+		},
+		testsData{
+			capacity: 5,
+			data: testData{
+				{"A", "a", []string{"A"}},
+				{"B", "b", []string{"A", "B"}},
+				{"C", "c", []string{"A", "B", "C"}},
+				{"D", "d", []string{"A", "B", "C", "D"}},
+				{"E", "e", []string{"A", "B", "C", "D", "E"}},
+			},
+		},
 	}
 
 	for _, tt := range tests {
-		lru.Set(tt.key, tt.value)
-		if lru.list.Len() > int(lru.capacity) {
-			t.Errorf("the LRU shouldn't be bigger than each individial character added. capacity: %v, actual length: %v", lru.capacity, lru.list.Len())
-		}
+		t.Run(fmt.Sprintf("Capacity %v", tt.capacity), func(t *testing.T) {
+			lru := NewLRUCache(uint64(tt.capacity))
+			for _, d := range tt.data {
+				lru.Set(d.key, d.value)
+				if lru.list.Len() > int(lru.capacity) {
+					t.Errorf("the LRU shouldn't be bigger than each individial character added. capacity: %v, actual length: %v", lru.capacity, lru.list.Len())
+				}
 
-		for _, e := range tt.expectedKeys {
-			if el := lru.hash[e]; el == nil {
-				t.Errorf("element should exist but doesn't: %v", e)
+				for _, e := range d.expectedKeys {
+					if el := lru.hash[e]; el == nil {
+						t.Errorf("element should exist but doesn't: %v", e)
+					}
+				}
 			}
-		}
+		})
 	}
 }
 
 func TestLRUClear(t *testing.T) {
 	lru := NewLRUCache(5)
 
-	tests := []struct {
+	data := []struct {
 		key   string
 		value string
 	}{
@@ -91,8 +186,8 @@ func TestLRUClear(t *testing.T) {
 		{"E", "e"},
 	}
 
-	for _, tt := range tests {
-		lru.add(tt.key, tt.value)
+	for _, dt := range data {
+		lru.add(dt.key, dt.value)
 	}
 
 	lru.Clear()
@@ -109,7 +204,7 @@ func TestLRUClear(t *testing.T) {
 func TestLRUCapacityClear(t *testing.T) {
 	lru := NewLRUCache(2)
 
-	tests := []struct {
+	data := []struct {
 		key   string
 		value string
 	}{
@@ -120,15 +215,15 @@ func TestLRUCapacityClear(t *testing.T) {
 		{"E", "e"},
 	}
 
-	for _, tt := range tests {
-		lru.add(tt.key, tt.value)
+	for _, dt := range data {
+		lru.add(dt.key, dt.value)
 	}
 
 	if lru.size <= lru.capacity {
 		t.Errorf("LRU capacity should be exceeded by now. actual size: %v and capacity: %v", lru.size, lru.capacity)
 	}
 
-	if len(lru.hash) != len(tests) || lru.list.Len() != len(tests) {
+	if len(lru.hash) != len(data) || lru.list.Len() != len(data) {
 		t.Error("LRU map and list should have the same length as the tests")
 	}
 
@@ -136,6 +231,41 @@ func TestLRUCapacityClear(t *testing.T) {
 
 	if lru.size != lru.capacity {
 		t.Error("LRU size should be limited to capacity now")
+	}
+}
+
+func TestLRUCacheSerialization(t *testing.T) {
+	lru := NewLRUCache(5)
+	data := []struct {
+		key   string
+		value string
+	}{
+		{"A", "a"},
+		{"B", "b"},
+		{"C", "c"},
+		{"D", "d"},
+		{"E", "e"},
+	}
+
+	for _, dt := range data {
+		lru.Set(dt.key, dt.value)
+	}
+
+	bb := &bytes.Buffer{}
+	if err := lru.saveCache(bb); err != nil {
+		t.Errorf("error in saving cache %v", err)
+	}
+
+	if b := bb.Bytes(); b == nil || len(string(b)) == 0 {
+		t.Error("bytes of buffer seems to be empty")
+	}
+
+	if d, err := lru.loadFromReader(bb); err != nil {
+		t.Errorf("error loading LRU from byte buffer: %v", err)
+	} else {
+		if len(d) != len(lru.hash) {
+			t.Errorf("length of deserialized LRU is different. Deserialized %v, Expected %v", len(d), len(lru.hash))
+		}
 	}
 }
 
